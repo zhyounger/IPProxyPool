f73e73119e499141159c94e328ceea6a464bb6bf
Orient <orient.zy@gmail.com>
2017-04-23 14:22:55 +0800

    Version 4.23
    IPProxyPool

---
+++ IPProxyPool.py
@@ -0,0 +1,64 @@
+import requests
+import re
+import random
+import time
+
+
+class IPProxyPool:
+
+    def __init__(self):
+        self.ip_list = []
+        self.ip_list_washed = []
+        self.user_agent_list = [
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+            "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+        ]
+
+    def get_ip_list(self, haoip_url = 'http://www.haoip.cc/tiqu.htm'):
+        html = requests.get(haoip_url)
+        ip_listn = re.findall(r'r/>(.*?)<b', html.text, re.S)
+        for ipn in ip_listn:
+            ip = re.sub('\n', '', ipn)
+            statu = self.check_ip(ip)
+            print(statu)
+            if statu:
+                self.ip_list.append(ip.strip())
+            print(self.ip_list)
+
+    def check_ip(self, ip):
+        test_url = 'https://www.baidu.com'
+        proxy = {'http': ip}
+        user_agent = random.choice(self.user_agent_list)
+        headers = {'User-Agent': user_agent}
+        try:
+            response = requests.get(test_url, headers=headers, proxies=proxy, timeout=5)
+            time.sleep(5)
+            if response.status_code == 200:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(e)
+            return False
+            time.sleep(5)
+
+
+
+IPProxyPool = IPProxyPool()
+IPProxyPool.get_ip_list()
