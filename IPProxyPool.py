6304645dcbdb57bfbd851f5eeb6139ce0b50cdea
Orient <orient.zy@gmail.com>
2017-04-23 14:34:44 +0800

    Bug Fix
    Bug Fix

--- IPProxyPool.py
+++ IPProxyPool.py
@@ -8,7 +8,7 @@ class IPProxyPool:
 
     def __init__(self):
         self.ip_list = []
-        self.ip_list_washed = []
+        # self.ip_list_washed = []
         self.user_agent_list = [
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
             "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
